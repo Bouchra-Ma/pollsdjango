b6fef78681b6f5d684c019f00f82ad74a8f706e9
Bouchra Matrah <bouchra.matrah@dadi-tech.net>
2025-07-07 16:39:23 +0200

    namespace

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,21 @@
-
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
 
-
 def index(request):
-    return render(request, "polls/index.html")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # ici, traite le vote (simplifié)
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
