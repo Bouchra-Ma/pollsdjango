73e307002cc028d1358d40f298eb396eb30fdd60
Bouchra Matrah <bouchra.matrah@dadi-tech.net>
2025-07-07 14:57:28 +0200

    methode 404

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
     return render(request, "polls/index.html")
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
