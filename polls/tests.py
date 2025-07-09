7508612b76062fe756ca1d53cb70b886db3544d7
Bouchra Matrah <bouchra.matrah@dadi-tech.net>
2025-07-09 10:24:35 +0200

    test views

--- polls/tests.py
+++ polls/tests.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-
+from django.urls import reverse
 from polls.models import Question
 
 class QuestionModelTests(TestCase):
@@ -14,3 +14,12 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """Si aucune question n'existe, afficher un message approprié."""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Aucune question disponible.")
+        self.assertEqual(len(response.context["latest_question_list"]), 0)
+
